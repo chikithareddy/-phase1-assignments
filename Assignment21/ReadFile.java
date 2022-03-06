82db7d10ae888998675715217cedb225ee42ed39
chikithareddy <chikithavutukuri456@gmail.com>
2022-03-07 00:59:24 +0530

    added changes

---
+++ Assignment21/ReadFile.java
@@ -0,0 +1,30 @@
+package Assignment21;
+
+	import java.io.File;
+	import java.io.FileReader;
+
+	public class ReadFile {
+	  public static void main(String[] args) {
+
+	    char[] array = new char[60];
+	    
+	    try {
+	      // Step 1: Creates a reader using the FileReader
+	      FileReader input = new FileReader("C:\\Users\\chikitha\\text");
+
+	      // Step 2: Reads characters
+	      input.read(array);
+	      
+	      System.out.println("Data in the file:");
+	      System.out.println(array);
+
+	      // Step 3: Closes the reader
+	      input.close();
+	    }
+	    catch(Exception e) {
+	      e.getStackTrace();
+	    }
+	  }
+	}
+
+
